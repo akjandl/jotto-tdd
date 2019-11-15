3843446a5bf2394170d653179fb1d20beb94b89e
Andy Jandl <akjandl@gmail.com>
2019-11-15 10:38:59 -0600

    Set up redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,11 @@
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - action to be reduced.
+ * @returns {boolean} - new success state.
+ */
+const reducer = (state, action) => {
+  return null;
+};
+
+export default reducer;
